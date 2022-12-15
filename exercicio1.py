1cfb7727137914e5fb3118cef8b69135bdc1d03d
Gilberto Pavuna <gilbertopavunacosta@hotmail.com>
2022-12-15 17:13:20 -0300

    Exercicio 1 atualizado

--- exercicio1.py
+++ exercicio1.py
@@ -1,6 +1,11 @@
-try:
-    nota = float(input('Entre com sua nota: '))
-except ValueError:
-    print('Digite um valor de 0 a 10.')
-else:
-    print(f'Sua nota é {nota}.')
+while True:
+    try:
+        nota = int(input('Entre com sua nota: '))
+    except ValueError:
+        print('Digite um valor inteiro')
+    else:
+        if 0 <= nota <= 10:
+            print(f'Sua nota é {nota}.')
+            break
+        else:
+            print('Digite um valor de 0 a 10.')
--- exercicio2.py
+++ exercicio2.py
@@ -1,4 +1,8 @@
-nome = input('Entre com o seu nome: ')
-senha = int(input('Entre com sua senha: '))
-if nome == senha:
-    print
+try:
+    nome = input('Entre com o seu nome: ')
+    senha = int(input('Entre com sua senha: '))
+except ValueError:
+    if senha == nome:
+        print('Senha não pode ser igual ao nome.')
+else:
+    print(f'{nome}, sua senha foi cadastrada.')
